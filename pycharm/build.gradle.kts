a7b23181085664a09f58030ece33fb22ce72b2ec
Dylan <dylan.siowlee@auroraer.com>
2026-03-11 15:12:04 +0000

    chore(pycharm): scaffold Gradle project structure and plugin.xml

---
+++ pycharm/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+}
+
+group = "com.gotchi"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1")
+    type.set("IC")   // IntelliJ Community
+    plugins.set(listOf())
+    updateSinceUntilBuild.set(false)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+}
